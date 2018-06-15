04ce6fae2140c8fceb47224ede4ae6ccfa3e59bb
Morris <dragongogoo@hotmail.com>
2018-06-15 15:08:50 +0200

    kk

--- src/Display/HealthBarDisplay.java
+++ src/Display/HealthBarDisplay.java
@@ -3,9 +3,11 @@ package Display;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.border.EtchedBorder;
 
 /**
  *
@@ -13,9 +15,11 @@ import javax.swing.JPanel;
  */
 public class HealthBarDisplay extends JPanel {
     //This panel displays players' health bar
-    private final int HEALTH_BAR_LENGTH = 200;
-    private final int HEALTH_BAR_HEIGHT = 20;
-    private final int HEIGHT = 20;//height between the label and the hp bar
+    //The panel's height is MARGIN+COMPONENT_HEIGHT+GAP_HEIGHT+COMPONENT_HEIGHT+MARGIN = 55
+    //The panel's length is MARGIN+COMPONENT_LENGTH+MARGIN = 210
+    private final int COMPONENT_LENGTH = 200;
+    private final int COMPONENT_HEIGHT = 20;
+    private final int GAP_HEIGHT = 5;//height between the label and the hp bar
     private final int MARGIN = 5;
     private int healthPoint;
     
@@ -23,9 +27,11 @@ public class HealthBarDisplay extends JPanel {
         this.healthPoint = 100;
         JLabel labelPseudo = new JLabel(pseudo);
         labelPseudo.setFont(new Font("Serif", Font.ROMAN_BASELINE, 25));
-        labelPseudo.setBounds(MARGIN,0, HEALTH_BAR_LENGTH, HEALTH_BAR_HEIGHT);
+        labelPseudo.setBounds(MARGIN,0, COMPONENT_LENGTH, COMPONENT_HEIGHT);
         add(labelPseudo);
-        setSize(HEALTH_BAR_LENGTH+2*MARGIN,HEALTH_BAR_HEIGHT+HEIGHT+MARGIN+MARGIN);
+        setSize(COMPONENT_LENGTH+2*MARGIN,2*COMPONENT_HEIGHT+GAP_HEIGHT+2*MARGIN);
+        this.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED));
+        this.setBackground(new Color(175,167,167));
         setLayout(null);
         setVisible(true);
     }
@@ -33,23 +39,23 @@ public class HealthBarDisplay extends JPanel {
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         g.setColor(Color.BLACK);
-        g.drawRoundRect(MARGIN, MARGIN+HEIGHT, HEALTH_BAR_LENGTH, HEALTH_BAR_HEIGHT,5,5); //draws healthbar outline
+        g.drawRoundRect(MARGIN, COMPONENT_HEIGHT+GAP_HEIGHT, COMPONENT_LENGTH, COMPONENT_HEIGHT,5,5); //draws healthbar outline
         g.setColor(Color.red);
-        g.fillRect(MARGIN, MARGIN+HEIGHT, (int)healthPoint*HEALTH_BAR_LENGTH/100, HEALTH_BAR_HEIGHT); //draws health       
+        g.fillRect(MARGIN, COMPONENT_HEIGHT+GAP_HEIGHT, (int)(healthPoint*COMPONENT_LENGTH/100), COMPONENT_HEIGHT); //draws health       
     }
     
     public void setHealthPoint(int healthPoint){
         //Repaint the hp bar every time it is modified
         this.healthPoint = healthPoint;
         repaint();
-    }
+    }  
     
     public static void main(String[] args){
         JFrame frame = new JFrame();
         frame.setBounds(0, 0, 1000, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(null);
-        frame.setVisible(true);
+        frame.setVisible(true);        
         HealthBarDisplay ss = new HealthBarDisplay("ss");
         ss.setLocation(0, 0);
         frame.add(ss);
@@ -57,4 +63,5 @@ public class HealthBarDisplay extends JPanel {
         sss.setLocation(0,50);
         frame.add(sss);
     }
+    
 }
