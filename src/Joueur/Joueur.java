70b4450743c2c7aa1c77418b5813397e5055fd6e
remianger <37573821+remianger@users.noreply.github.com>
2018-06-16 11:58:10 +0200

    Update Joueur.java

--- src/Joueur/Joueur.java
+++ src/Joueur/Joueur.java
@@ -37,7 +37,7 @@ public class Joueur {
         if ("America".equals(nation)){
             arme = "M16";
         } else if ("Terrorist".equals(nation)){
-            arme = "Gun";
+            arme = "AK47";
         }
         pv = 0;
         vitesse = 1; //joueur se déplace case par case par défaut
@@ -56,7 +56,7 @@ public class Joueur {
         if ("America".equals(nation)){
             arme = "M16";
         } else if ("Terrorist".equals(nation)){
-            arme = "Gun";
+            arme = "AK47";
         }
         this.pv = pv;
         vitesse = 1; //joueur se déplace case par case par défaut
